dc86fdd13abf31ea9df010ac44b8b943fb798ff0
omotosho olushola mary <101481226+shollybee@users.noreply.github.com>
2022-06-28 05:55:41 +0100

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,32 +1,26 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
-  * check_cycle - Checks if a singly linked list has a cycle in it
-  * @list: The singly linked list to check
-  *
-  * Return: 1 if has a cycle in it or 0 if not
-  */
+ * check_cycle - checks to see if a list is in an endless loop or cycle
+ * @list: the list to check
+ * Return: 0 if no cycle is detected, 1 if there is a cycle
+ */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle = list, *hare = list;
-	int found = 0;
+	listint_t *doub = list;
+	listint_t *reg = list;
 
-	while ((turtle && hare) && hare->next)
-	{		
-		turtle = turtle->next;
+	if (list == NULL)
+		return (0);
 
-		if (hare->next || hare->next->next)	
-			hare = hare->next->next;
-		else
-			break;
+	while (doub && doub->next)
+	{
+		reg = reg->next;
+		doub = doub->next->next;
 
-		if (turtle == hare)
-		{
-			found = 1;
-			break;
-		}
+		if (reg == doub)
+			return (1);
 	}
-
-	return (found);
+	return (0);
 }
